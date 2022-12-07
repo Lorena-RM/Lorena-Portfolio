696c981fb75c68af14041b4e2c04b71ae211b5c3
Lorena Morales <Lorenarm.999@gmail.com>
2022-12-06 18:29:38 -0700

    links to projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,37 +6,37 @@ export default function Projects() {
     <>
       <MDBRipple id="padding" rippleTag='div' className='sm-image hover-overlay hover-zoom hover-shadow'>
       <img src={require('./assets/hi-blog-im-dad.png')} className='w-100' />
-      <a href='#!'>
+      <a href='https://hi-blog-im-dad.herokuapp.com/login'>
         <div className='mask' style={{ backgroundColor: 'rgba(57, 192, 237, 0.2)' }}></div>
       </a>
     </MDBRipple>
     <MDBRipple id="padding" rippleTag='div' className='sm-image hover-overlay hover-zoom hover-shadow'>
       <img src={require('./assets/tarotkeeper.png')} className='w-100' />
-      <a href='#!'>
+      <a href='https://my-tarot-keeper.herokuapp.com/'>
         <div className='mask' style={{ backgroundColor: 'rgba(57, 192, 237, 0.2)' }}></div>
       </a>
     </MDBRipple>
     <MDBRipple id="padding" rippleTag='div' className='sm-image hover-overlay hover-zoom hover-shadow'>
       <img src={require('./assets/deptFunctions.gif')} className='w-100' />
-      <a href='#!'>
+      <a href='https://github.com/Lorena-RM/my-Employee-Tracker'>
         <div className='mask' style={{ backgroundColor: 'rgba(57, 192, 237, 0.2)' }}></div>
       </a>
     </MDBRipple>
     <MDBRipple id="padding" rippleTag='div' className='sm-image hover-overlay hover-zoom hover-shadow'>
       <img src={require('./assets/project1.png')} className='w-100' />
-      <a href='#!'>
+      <a href='https://alecschro13.github.io/Venue-Search-Project1/'>
         <div className='mask' style={{ backgroundColor: 'rgba(57, 192, 237, 0.2)' }}></div>
       </a>
     </MDBRipple>
     <MDBRipple id="padding" rippleTag='div' className='sm-image hover-overlay hover-zoom hover-shadow'>
       <img src={require('./assets/workdaySched.png')} className='w-100' />
-      <a href='#!'>
+      <a href='https://lorena-rm.github.io/work-day-scheduler-jquery-lm/'>
         <div className='mask' style={{ backgroundColor: 'rgba(57, 192, 237, 0.2)' }}></div>
       </a>
     </MDBRipple>
     <MDBRipple id="padding" rippleTag='div' className='sm-image hover-overlay hover-zoom hover-shadow'>
       <img src={require('./assets/Screen-Recording.gif')} className='w-100' />
-      <a href='#!'>
+      <a href='https://lorena-rm.github.io/Weather-Dashboard-Morales/'>
         <div className='mask' style={{ backgroundColor: 'rgba(57, 192, 237, 0.2)' }}></div>
       </a>
     </MDBRipple>
